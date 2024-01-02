ea9ca24e91fcd8d79277492ca8c8144e1f3ae02a
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2024-01-02 12:39:18 -0800

    fixed task 5 and 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for n in range(100):
     if n != 89:
         if (n % 10) > (n / 10):
-            print(f"{n:02d}, ", end = "")
+            print("{:02d}, ".format(n), end="")
     else:
         print(n)
