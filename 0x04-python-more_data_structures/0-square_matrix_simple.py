3b1c423f2ee4ef52ccc60dad49f04b17459dcbf0
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2024-01-20 13:13:18 -0800

    fixed pycodestyle

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ def square_matrix_simple(matrix=[]):
         return list(map(lambda a: a*a, x))
     return list(map(squaring, matrix))
 
-    #return list(map(lambda x: list(map(lambda a: a*a, x)), matrix))
-    #return list(map(lambda x: list(map(lambda a: a*a, x))), matrix)
+    # return list(map(lambda x: list(map(lambda a: a*a, x)), matrix))
+    # return list(map(lambda x: list(map(lambda a: a*a, x))), matrix)
