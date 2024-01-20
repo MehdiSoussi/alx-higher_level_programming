b2f2c489a6e63edfba2213c36212c0c3963720be
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2024-01-20 15:18:28 -0800

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    else:
+        max = 0
+        keymax = None
+        for key, value in a_dictionary.items():
+            if value >= max:
+                keymax = key
+    return keymax
