47b3a560eda7e58cd0807a1fcf611d5086beab9d
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2024-01-24 12:19:00 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ python script """
 
+
 class Square():
     """Class representing a square with an attribute private size"""
-    
+
     def __init__(self, size):
         """ Initializing the instances"""
-        
+
         self.__size = size
