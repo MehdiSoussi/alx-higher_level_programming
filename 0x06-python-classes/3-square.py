8ba2c4370a213aa27dcf8b17ebc977518d7d4e74
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2024-01-24 12:31:31 -0800

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" python script """
+
+
+class Square():
+    """Class representing a square with an attribute private size"""
+
+    def __init__(self, size=0):
+        """ Initializing the instances"""
+        if isinstance(size, int):
+            if size > 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+    def area(self):
+        return self.__size * self.__size
